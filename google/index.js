8c7ff26528564876d85a0304bcc29467a37de22e
Linda Nichols <lynnaloo@gmail.com>
2020-02-24 22:54:34 -0500

    formatting on webpages

--- google/index.js
+++ google/index.js
@@ -1,7 +1,10 @@
 
 exports.best = (request, response) => {
-  console.log('show the best of 1988');
-  response.status(200).send('');
+  const html = `<html><body>
+    <h1>The Best of 1988</h1>
+    <iframe src="https://giphy.com/embed/FbPsiH5HTH1Di" width="480" height="360" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+    </body></html>`;
+  response.status(200).send(html);
 };
 
 exports.event = (event, callback) => {
--- microsoft/handlers/handler.js
+++ microsoft/handlers/handler.js
@@ -1,10 +1,9 @@
 /* eslint-disable require-await */
 
 module.exports.best = async function(context, req) {
-  context.log('show the best of 1988');
-  const html = `<html><body> +
-    <h1>The Best of 1988</h1> +
-    <iframe src="https://giphy.com/embed/FWi1f9Wn2hubC" width="480" height="360" frameBorder="0" class="giphy-embed" allowFullScreen></iframe> +
+  const html = `<html><body>
+    <h1>The Best of 1988</h1>
+    <iframe src="https://giphy.com/embed/olAik8MhYOB9K" width="480" height="360" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
     </body></html>`;
 
   context.res = {

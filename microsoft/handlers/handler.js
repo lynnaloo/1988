db1a1cfcd7e29a2e37c8948ee6fdf0533757da93
Linda Nichols <lynnaloo@gmail.com>
2020-02-26 21:48:33 -0600

    Add text to function output

--- microsoft/handlers/handler.js
+++ microsoft/handlers/handler.js
@@ -2,7 +2,7 @@
 
 module.exports.best = async function(context, req) {
   const html = `<html><body>
-    <h1>This is the Best of 1988</h1>
+    <h1>The Best of 1988</h1>
     <iframe src="https://giphy.com/embed/olAik8MhYOB9K" width="480" height="360" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
     </body></html>`;
 
